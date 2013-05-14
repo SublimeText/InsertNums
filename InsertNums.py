cd969077c340991177d783903ea9ea0874864302
James Brooks <jbrooksuk@me.com>
2013-05-14 10:48:18 +0100

    Added Command Palette item

--- InsertNums.py
+++ InsertNums.py
@@ -2,14 +2,14 @@ import sublime, sublime_plugin
 
 class PromptInsertNumsCommand(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel('Enter a starting number and step.', '1 1 1', self.on_done, None, None)
+        self.window.show_input_panel('Enter a starting number, step and padding.', '1 1 1', self.on_done, None, None)
         pass
 
     def on_done(self, text):
         try:
             (current, step, padding) = map(str, text.split(" "))
             if self.window.active_view():
-                self.window.active_view().run_command("insert_nums", {"current" : current, "step" : step, "padding": padding} )
+                self.window.active_view().run_command("insert_nums", {"current": current, "step": step, "padding": padding} )
         except ValueError:
             pass
 
