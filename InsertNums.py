SublimeText/InsertNums

commit a99f132f686c19499e06217d8ad3d9bfec9c6560

Add option to fill the regions in reversed order

InsertNums.py
@@ -9,8 +9,9 @@ if int(sublime.version()) > 3000:
 
 module_name = "InsertNums"
 
-# TODO: option to do stuff backwards (yet another "option")
-# TODO: expression for "step" (e.g. "_**2")
+
+# TODO: expressions for "step" (e.g. "_**2")
+# TODO: expressions only for the value, independant of the actual "step"
 
 
 def strip_line_spaces(string):
@@ -65,12 +66,14 @@ def get_rexexps(*ret):
         # finals
         insertnum     ::=  ^ (?P<start> {signednum})
                            (: (?P<step> {signednum}) )?
-                           (~ (?P<format> {format}) )? $
+                           (~ (?P<format> {format}) )?
+                           (?P<reverse> !)? $
 
         insertalpha   ::=  ^ (?P<start> {alphastart})
                            (: (?P<step> {signedint}) )?
                            (~ (?P<format> {alphaformat})
-                              (?P<wrap> w)? )? $
+                              (?P<wrap> w)? )?
+                           (?P<reverse> !)? $
     """
 
     # Our most important variable
@@ -125,6 +128,11 @@ class InsertNumsCommand(sublime_plugin.TextCommand):
         step   = int_or_float(m['step']) if m['step'] else 1
         format = m['format']
 
+        # Reverse the regions if requested
+        selections = [reg for reg in self.view.sel()]
+        if m['reverse']:
+            selections.reverse()
+
         # Do the stuff
         if not ALPHA:
             value = start
@@ -133,7 +141,7 @@ class InsertNumsCommand(sublime_plugin.TextCommand):
                     and isinstance(value, int)):
                 value = float(value)
 
-            for region in self.view.sel():
+            for region in selections:
                 if format:
                     replace = "{value:{format}}".format(value=value, format=format)
                 else:
@@ -149,7 +157,7 @@ class InsertNumsCommand(sublime_plugin.TextCommand):
             lenght = len(start) if WRAP else 0
             value = self.alpha_to_num(start.lower())
 
-            for region in self.view.sel():
+            for region in selections:
                 replace = self.num_to_alpha(value, lenght)
                 if UPPER:
                     replace = replace.upper()

commit cb750dfe201cde7610a95ed23cc94b98e94159c3

Regression fix for the "reverse" addition
Introduced by a99f132f686c19499e06217d8ad3d9bfec9c6560.

`self.view.sel()` in fact returns an interator (or, "RegionSet"s behave like
iterators) which would adapt to changes from `view.replace()`. However, if we
construct the region list before making the changes this will be skipped.

InsertNums.py
@@ -128,10 +128,12 @@ class InsertNumsCommand(sublime_plugin.TextCommand):
         step   = int_or_float(m['step']) if m['step'] else 1
         format = m['format']
 
-        # Reverse the regions if requested
-        selections = [reg for reg in self.view.sel()]
+        # Reverse the regions if requested | by default, this works like an iterator
+        selections = self.view.sel()
         if m['reverse']:
-            selections.reverse()
+            # Construct a real list and reverse that, won't affect the regions
+            # since we're going backwards
+            selections = reversed(reg for reg in selections)
 
         # Do the stuff
         if not ALPHA: